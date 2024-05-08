1c74c1760b7240c075e9c5f5f83b84d919fcfe08
Sameeer-Ahmad <sam9910333@gmail.com>
2024-05-08 21:27:15 +0530

    completed CRUD operations in backend

--- backend/routes/art.routes.js
+++ backend/routes/art.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { postArt, getArt, deleteArt } = require("../controller/art.controller");
+const { postArt, getArt, deleteArt, updateArt, getArtById } = require("../controller/art.controller");
 
 const artRouter = express.Router();
 
@@ -8,6 +8,10 @@ artRouter.post("/add", postArt);
 artRouter.get("/get", getArt);
 
 artRouter.delete("/delete/:id", deleteArt);
+
+artRouter.patch("/update/:id", updateArt);
+
+artRouter.get("/get/:id", getArtById);
 
 module.exports = {
   artRouter,
