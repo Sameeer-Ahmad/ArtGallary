971a7c085fff113deaf167cffa2b5a23e63a63e5
PrasadPawar2729 <prasadpawarpp2002@gmail.com>
2024-05-08 21:35:12 +0530

    creted all routes

--- frontend/ArtGallary/src/Router/AllRoutes.jsx
+++ frontend/ArtGallary/src/Router/AllRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import { BrowserRouter as  Route,Routes } from 'react-router-dom';
 import Navigation from '../Navigation/Navigate';
 import Paint from '../Component/Paint/Paint';
 import Print from '../Component/Prints/Print';
