3fa86b17be9198abb288bbdad7109e60ac093276
Sameeer-Ahmad <sam9910333@gmail.com>
2024-05-14 00:08:26 +0530

    make some  changes in backend

--- backend/model/cart.model.js
+++ backend/model/cart.model.js
@@ -4,7 +4,7 @@ const cartSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   artId: { type: String, required: true },
   quantity: { type: Number, required: true, default: 1 },
-  
+
   createdAt: { type: Date, default: Date.now },
 });
 
